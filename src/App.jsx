ac22ce6d970f3ef323aef78a8023c4b7d0ff448a
Bazkiller <ohm.ratchaphon@gmail.com>
2025-05-05 03:56:02 +0700

    create education section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar.jsx";
 import Skill from "./components/Skill.jsx";
 import Project from "./components/Project.jsx";
 import Experience from "./components/Experience.jsx";
+import Education from "./components/Education.jsx";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Project />
       <div>
         <Experience />
+        <Education />
       </div>
     </div>
   );
---
+++ src/components/Education.jsx
@@ -0,0 +1,20 @@
+const Education = () => {
+  return (
+    <section className="education">
+      <h2>Education</h2>
+      <div className="e-card">
+        <p className="date">July 2024 - Present</p>
+        <h3 className="e-title">
+          King Mongkut’s Insitude of Technology LadKrabang
+        </h3>
+        <p className="e-descr">
+          Bachelor of Science Program in Information Technology,
+          <span>3.63 GPA</span>
+        </p>
+        <div className="e-descr">Major in Information Technology</div>
+      </div>
+    </section>
+  );
+};
+
+export default Education;
