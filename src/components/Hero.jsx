aa0d4798499b01dbdea03e7a3291dd89c397bec6
Bazkiller <ohm.ratchaphon@gmail.com>
2025-05-05 06:16:25 +0700

    add: add style for hero and make it responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,33 +1,41 @@
 const Hero = () => {
   return (
-    <section className="hero-section">
-      <div className="hero-text">
-        <p>Hi, I’m</p>
-        <h1>Ratchaphon Khawkhiew</h1>
-        <h2>I’m a Full-Stack Developer.</h2>
-        <p>
-          I am a full-stack developer with a passion for creating dynamic and
-          responsive web applications. I have experience in both front-end and
-          back-end development, and I am always eager to learn new technologies
-          and improve my skills.
-        </p>
-      </div>
-      <div className="contacts">
-        <a href="" className="button">
-          View Resume
-        </a>
-        <a>
-          <img src="./#" alt="Email_Icon" />
-        </a>
-        <a>
-          <img src="./#" alt="Tel_Icon" />
-        </a>
-        <a>
-          <img src="./#" alt="Github_Icon" />
-        </a>
-      </div>
-      <div className="hero-image">
-        <img src="#" alt="Hero" />
+    <section className="hero">
+      <div className="hero-content">
+        <div className="hero-text">
+          <p className="sub-hero">Hi, I’m</p>
+          <h1 className="hero-name gradient-purple">Ratchaphon Khawkhiew</h1>
+          <h2 className="sub-hero">I’m a Full-Stack Developer.</h2>
+          <p className="hero-description">
+            I am a full-stack developer with a passion for creating dynamic and
+            responsive web applications. I have experience in both front-end and
+            back-end development, and I am always eager to learn new
+            technologies and improve my skills.
+          </p>
+          <div className="hero-contacts">
+            <a
+              href="https://drive.google.com/file/d/1vpEB8iUWMB6zYPb9iBrDYfnTfkQ7OX19/view?usp=drive_link"
+              target="_blank"
+              className="resume-btn"
+            >
+              View Resume
+            </a>
+            <div className="hero-contacts-icons">
+              <a href="#" target="_blank">
+                <img src="/icons/email.png" alt="Email_Icon" />
+              </a>
+              <a href="#" target="_blank">
+                <img src="/icons/tel.png" alt="Tel_Icon" />
+              </a>
+              <a href="https://github.com/BazkilleR" target="_blank">
+                <img src="/icons/github.png" alt="Github_Icon" />
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="hero-img">
+          <img src="#" alt="Hero Image" />
+        </div>
       </div>
     </section>
   );
