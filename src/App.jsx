32284c8484721afdc718f4581ca565a0249fe7a9
Bazkiller <ohm.ratchaphon@gmail.com>
2025-05-05 02:57:09 +0700

    create skills section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import "./App.css";
 import Hero from "./components/Hero.jsx";
 import Navbar from "./components/Navbar.jsx";
+import Skill from "./components/Skill.jsx";
 
 function App() {
   return (
     <div>
-      <Navbar />
-      <Hero />
+      <section className="hero-section">
+        <Navbar />
+        <Hero />
+      </section>
+      <section className="skill">
+        <Skill />
+      </section>
     </div>
   );
 }
---
+++ src/components/Skill.jsx
@@ -0,0 +1,70 @@
+const Skill = () => {
+  return (
+    <div>
+      <h2>My Skills</h2>
+      <div className="skill-group">
+        <h3>Programming Language</h3>
+        <ul>
+          <li className="skill">
+            <img src="#" alt="HTML_Icon" />
+          </li>
+          <li className="skill">
+            <img src="#" alt="CSS_Icon" />
+          </li>
+          <li className="skill">
+            <img src="#" alt="JavaScript_Icon" />
+          </li>
+          <li className="skill">
+            <img src="#" alt="PHP_Icon" />
+          </li>
+          <li className="skill">
+            <img src="#" alt="Python_Icon" />
+          </li>
+          <li className="skill">
+            <img src="#" alt="Java_Icon" />
+          </li>
+          <li className="skill">
+            <img src="#" alt="SQL_Icon" />
+          </li>
+        </ul>
+      </div>
+      <div className="skill-group">
+        <h3>Frameworks & Libraries</h3>
+        <ul>
+          <li className="skill">
+            <img src="#" alt="React_Icon" />
+          </li>
+          <li className="skill">
+            <img src="#" alt="Tailwind_Icon" />
+          </li>
+          <li className="skill">
+            <img src="#" alt="Bootstrap_Icon" />
+          </li>
+          <li className="skill">
+            <img src="#" alt="Flask_Icon" />
+          </li>
+        </ul>
+      </div>
+      <div className="skill-group">
+        <h3>Tools & Platforms</h3>
+        <ul>
+          <li className="skill">
+            <img src="#" alt="Github_Icon" />
+          </li>
+          <li className="skill">
+            <img src="#" alt="Postman_Icon" />
+          </li>
+          <li className="skill">
+            <img src="#" alt="Hostinger_Icon" />
+          </li>
+          <li className="skill">
+            <img src="#" alt="Vercel_Icon" />
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Skill;
+<div>Hello</div>;
