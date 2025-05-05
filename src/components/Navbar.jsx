992f7a5753b7e17e8095c5ca4dce51c6fd3dccf2
Bazkiller <ohm.ratchaphon@gmail.com>
2025-05-05 16:50:54 +0700

    add: make mobile navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,60 @@
+import { useState } from "react";
+
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <nav>
       <div className="nav-content">
         <div className="nav-logo">PORTFOLIO</div>
-        <div className="nav-menu">
+        <div className="hamburger" onClick={toggleMenu}>
+          <img src="/icons/burger_nav.png" alt="burger_nav" />
+        </div>
+        <div className="nav-menu" id="navMenu">
           <ul>
             <li>
-              <a href="http://" target="#" rel="noopener noreferrer">
-                HOME
-              </a>
+              <a href="#">HOME</a>
             </li>
             <li>
-              <a href="http://" target="#" rel="noopener noreferrer">
-                SKILLS
-              </a>
+              <a href="#">SKILLS</a>
             </li>
             <li>
-              <a href="http://" target="#" rel="noopener noreferrer">
-                PROJECT
-              </a>
+              <a href="#">PROJECT</a>
             </li>
             <li>
-              <a href="http://" target="#" rel="noopener noreferrer">
-                EXPERIENCE
-              </a>
+              <a href="#">EXPERIENCE</a>
             </li>
           </ul>
         </div>
+      </div>
+      <div className={`mobile-nav-menu ${showMenu ? "show" : ""}`} id="navMenu">
+        <ul>
+          <li>
+            <a href="#">
+              <div className="mb-nav">HOME</div>
+            </a>
+          </li>
+
+          <li>
+            <a href="#">
+              <div className="mb-nav">SKILLS</div>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <div className="mb-nav">PROJECT</div>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <div className="mb-nav">EXPERIENCE</div>
+            </a>
+          </li>
+        </ul>
       </div>
     </nav>
   );
